501be6c1de20bddc52a245931117d78a23a8077a
jay-lchicken <167660133+jay-lchicken@users.noreply.github.com>
2024-05-31 11:55:09 +0800

    Update firebase-auth.js

--- Rewards main/firebase-auth.js
+++ Rewards main/firebase-auth.js
@@ -40,7 +40,7 @@ function login() {
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      alert("Error: Incorrect Password");
+      alert("Error: Invalid Credentials");
     });
 }
 function getCurrentUserId() {
